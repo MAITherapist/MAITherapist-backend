ac323c3a7af05f8cc7ecf86b7b2fa246909ce4cf
Uğur kellecioğlu <ugur.kellecioglu@outlook.com>
2023-02-19 01:12:44 +0300

    feat: create patient authentication

--- createPatient/index.js
+++ createPatient/index.js
@@ -1,6 +1,11 @@
 const AWS = require("aws-sdk")
 const dynamoDb = new AWS.DynamoDB.DocumentClient()
-
+const { CognitoJwtVerifier } = require("aws-jwt-verify")
+const verifier = CognitoJwtVerifier.create({
+  userPoolId: process.env.userPoolId,
+  tokenUse: "access",
+  clientId: process.env.clientId,
+})
 exports.handler = async (event, context) => {
   const patient = JSON.parse(event.body)
 
@@ -10,6 +15,22 @@ exports.handler = async (event, context) => {
   }
 
   try {
+    if (event.httpMethod === "OPTIONS") {
+      return {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET,OPTIONS",
+          "Access-Control-Allow-Headers":
+            "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+        },
+        body: "",
+      }
+    }
+    const token = event.headers["Authorization"].split(" ")[1]
+
+    await verifier.verify(token)
+
     await dynamoDb.put(params).promise()
     return {
       statusCode: 200,
@@ -22,6 +43,10 @@ exports.handler = async (event, context) => {
   } catch (error) {
     return {
       statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
       body: JSON.stringify({
         error: `Could not create patient: ${error.stack}`,
       }),
